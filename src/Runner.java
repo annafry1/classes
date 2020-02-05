5dbc9c246aa8465eddeda30521bc657b2fd15446
Anna Fry <annafry@students.berkeley.net>
2020-02-05 14:26:15 -0800

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Car myCar = new Car(2014, "Toyota", "Prius");
+        myCar.toString();
+        myCar.drive(400);
+        myCar.toString();
+
+    }
+}
